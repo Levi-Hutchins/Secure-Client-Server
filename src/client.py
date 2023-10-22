8c65893fd41d330392fd36b522547755e00810a0
Levi Hutchins <levihutchins0@gmail.com>
2023-10-22 12:31:21 +1100

    documentation

--- src/client.py
+++ src/client.py
@@ -1,33 +1,48 @@
+# Import necessary libraries
 import requests
 import base64
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import algorithms, modes
 from cryptography.hazmat.primitives.ciphers import Cipher
 import os
+
+# Define a secret key for symmetric encryption between client and server.
 SECRET_KEY = b'6TXPMrtJBnkiJ8mo'
-clientToken = 0
-current_token = None
+
+# Function to encrypt data before sending it to the server.
 def encrypt_before_transmission(encryptMe):
+    # Generate a random initialization vector (IV) for AES encryption
     iv = os.urandom(16)
+    
+    # Initialize the AES cipher with CFB mode
     cipher = Cipher(algorithms.AES(SECRET_KEY), modes.CFB(iv), backend=default_backend())
     encryptor = cipher.encryptor()
+    
+    # Encrypt the data
     ciphertext = encryptor.update(encryptMe.encode()) + encryptor.finalize()
-
-    # Base64 encode the ciphertext and IV
+    
+    # Encode the ciphertext and IV using base64 for safe transmission
     encoded_ciphertext = base64.b64encode(ciphertext).decode('utf-8')
     encoded_iv = base64.b64encode(iv).decode('utf-8')
 
     return encoded_ciphertext, encoded_iv
 
+# Function for admin login authentication
 def adminLogin(password):
+    # Encrypt the password
     sensitive = encrypt_before_transmission(password)
+    
+    # Construct the payload to be sent to the server
     loginPayload = {
         "username": "root",
         "password":  sensitive[0],
         "notIV": sensitive[1]
     }
-
+    
+    # Send a POST request to the server's admin login route
     r = requests.post("http://127.0.0.1:2250/admin_login", data=loginPayload).text
+    
+    # Check the server's response
     if r == "Access Granted": 
         print("\n ! Access Granted !\n")
         return True
@@ -35,17 +50,25 @@ def adminLogin(password):
         print("\n ! Access Denied !\n")
         return False
 
+# Function for user login, handling authentication, MFA, and token verification
 def userLogin(user_input):
+    # Encrypt the user's password
     sensitive = encrypt_before_transmission(user_input[2])
-    global current_token
+    
+    # Prepare the user's data for transmission to the server
     userData = {
         "username":user_input[1],
         "password": sensitive[0],
-        "notIV": sensitive[1] }
+        "notIV": sensitive[1] 
+    }
 
+    # Send the user's data to the server's user login route
     r = requests.post("http://127.0.0.1:2250/user_login",data=userData).text
+    
+    # Handle the server's response for MFA or token validation
     if r == "True":
         print("You were sent an email containing a verification code")
+        # Handle MFA
         code_input = input(">>> ")
         userData = {"username":user_input[1],"code": code_input}
         r = requests.post("http://127.0.0.1:2250/verify_login",data=userData).text
@@ -56,11 +79,10 @@ def userLogin(user_input):
             userData = {"username":user_input[1],"code": code_input}
             r = requests.post("http://127.0.0.1:2250/verify_login",data=userData).text
             print(r)
-    
     if r == "Token Valid":
         print("\n ! Access Granted !\n Your Token is still valid\n")
 
-    
+# UI
 def adminConsole():
     print("--------------------------")
     print("Admin Controls:")
@@ -71,11 +93,11 @@ def adminConsole():
     print("Delete User:  delete <username>")
     print("Log In:       login <username> <password>")
     print("--------------------------")
-    # These eight only demonstrate the implemented access control
+    # Calls all access control calls
     print("Test Endpoint Access Control: <test>")
     print("--------------------------")
     user_input = input(">>> ").strip().split(' ', 2)
-
+    # all required functions - send appropriate information
     if len(user_input) == 3 and user_input[0] == "add" :
         userData = {"username":user_input[1],"email_address": user_input[2]}
         r = requests.post("http://127.0.0.1:2250/admin/add_user", data=userData)
@@ -91,6 +113,7 @@ def adminConsole():
     if user_input[0] == "login":
        userLogin(user_input)
 
+    # Make calls to all endpoints to determine access level
     if user_input[0] == "test":
         print(requests.post("http://127.0.0.1:2250/view_roster").text)
         print(requests.post("http://127.0.0.1:2250/roster_shift").text)
@@ -102,6 +125,7 @@ def adminConsole():
         print(requests.post("http://127.0.0.1:2250/view_meeting_minutes").text)
         print(requests.post("http://127.0.0.1:2250/add_expense").text)
 
+# UI
 def userConsole():
     print("------------------------")
     print("User Controls:")
@@ -111,8 +135,8 @@ def userConsole():
     user_input = input(">>> ").strip().split(' ', 2)
     if user_input[0] == "login":
        userLogin(user_input)
-    # I have added all the possible endpoint to showcase that the access control is working correctly.
-    # The user Console is for users that are not admin so none of these will work unless have the required security level
+
+    # Make calls to all endpoints to determine access level
     if user_input[0] == "test":
         print(requests.post("http://127.0.0.1:2250/view_roster").text)
         print(requests.post("http://127.0.0.1:2250/roster_shift").text)
@@ -123,12 +147,14 @@ def userConsole():
         print(requests.post("http://127.0.0.1:2250/audit_timesheets").text)
         print(requests.post("http://127.0.0.1:2250/view_meeting_minutes").text)
         print(requests.post("http://127.0.0.1:2250/add_expense").text)
+
+# Determine admin status to display admin interface
 def isAdmin():
     if requests.get("http://127.0.0.1:2250/adminStatus").text == "True":
         return True
     else: return False
     
-
+# Main loop provides some error handling with the root password
 loopCount = 0
 if __name__ == "__main__":
     while True:
@@ -155,4 +181,3 @@ if __name__ == "__main__":
 
 
   
-
